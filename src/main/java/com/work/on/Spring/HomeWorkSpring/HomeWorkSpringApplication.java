6e272a30b908e1f6c0327421c43f2fffd5d4e44f
rinatagliulov <rinat.agliulov@rentready.com>
2024-03-23 12:24:11 -0400

    Start work

---
+++ src/main/java/com/work/on/Spring/HomeWorkSpring/HomeWorkSpringApplication.java
@@ -0,0 +1,13 @@
+package com.work.on.Spring.HomeWorkSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWorkSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWorkSpringApplication.class, args);
+	}
+
+}
